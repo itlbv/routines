8aa402a28733cf885fde0dc78d9ab609a7654a69
itlbv <artem.v.fokin@gmail.com>
2019-02-19 21:39:58 +0100

    Add Logback and SLF4J logging

--- src/main/java/com/itlbv/routines/Main.java
+++ src/main/java/com/itlbv/routines/Main.java
@@ -1,7 +1,14 @@
 package com.itlbv.routines;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         System.out.println("METHOD MAIN");
+        log.info("Example log");
     }
 }
