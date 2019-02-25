95165d3cd3d902fbfb13cfb19b39456bd354d67c
itlbv <artem.v.fokin@gmail.com>
2019-02-25 11:18:23 +0100

    Add MockRoutineRepository methods, fix timeOfDay field in Routine class

--- src/main/java/com/itlbv/routines/model/Routine.java
+++ src/main/java/com/itlbv/routines/model/Routine.java
@@ -1,6 +1,7 @@
 package com.itlbv.routines.model;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Routine extends AbstractBaseEntity {
 
@@ -8,10 +9,10 @@ public class Routine extends AbstractBaseEntity {
     private String description;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    private LocalDateTime timeOfDay;
+    private LocalTime timeOfDay;
     private boolean active;
 
-    public Routine(Integer id, String name, String description, LocalDateTime startTime, LocalDateTime endTime, LocalDateTime timeOfDay) {
+    public Routine(Integer id, String name, String description, LocalDateTime startTime, LocalDateTime endTime, LocalTime timeOfDay) {
         super(id, name);
         this.name = name;
         this.description = description;
@@ -19,5 +20,59 @@ public class Routine extends AbstractBaseEntity {
         this.endTime = endTime;
         this.timeOfDay = timeOfDay;
         this.active = true;
+    }
+
+    public boolean isNew() {
+        return (getId() == null);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalTime getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(LocalTime timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
--- src/main/java/com/itlbv/routines/repository/mock/MockRoutineRepositoryImpl.java
+++ src/main/java/com/itlbv/routines/repository/mock/MockRoutineRepositoryImpl.java
@@ -3,6 +3,10 @@ package com.itlbv.routines.repository.mock;
 import com.itlbv.routines.model.Routine;
 import com.itlbv.routines.repository.RoutineRepository;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -11,9 +15,10 @@ public class MockRoutineRepositoryImpl implements RoutineRepository {
     private ConcurrentHashMap<Integer, Routine> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
-    @Override
-    public Routine save(Routine user) {
-        return null;
+    private List<Routine> routinesForInitialPopulation = new ArrayList<>();
+
+    {
+        populateRepository();
     }
 
     @Override
@@ -29,5 +34,37 @@ public class MockRoutineRepositoryImpl implements RoutineRepository {
     @Override
     public List<Routine> getAll() {
         return null;
+    }
+
+    @Override
+    public Routine save(Routine routine) {
+        if (routine.isNew()) {
+            routine.setId(counter.incrementAndGet());
+            repository.put(routine.getId(), routine);
+            return routine;
+        }
+        return repository.computeIfPresent(routine.getId(), (id, oldRoutine) -> routine);
+    }
+
+    private void populateRepository() {
+        routinesForInitialPopulation.add(new Routine(1, "Routine 1", "Description 1",
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalTime.of(11, 0)));
+        routinesForInitialPopulation.add(new Routine(2, "Routine 2", "Description 2",
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalTime.of(11, 0)));
+        routinesForInitialPopulation.add(new Routine(3, "Routine 3", "Description 3",
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalTime.of(11, 0)));
+        routinesForInitialPopulation.add(new Routine(4, "Routine 4", "Description 4",
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
+                LocalTime.of(11, 0)));
+
+        routinesForInitialPopulation.forEach(this::save);
+        counter.set(repository.size());
     }
 }
