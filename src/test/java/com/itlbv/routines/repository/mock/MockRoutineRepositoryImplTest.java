099e67447f35bb07d4d7dd834eefbd30798742d5
itlbv <artem.v.fokin@gmail.com>
2019-02-25 11:18:58 +0100

    Add MockRoutineRepositoryTest boilerplate

---
+++ src/test/java/com/itlbv/routines/repository/mock/MockRoutineRepositoryImplTest.java
@@ -0,0 +1,22 @@
+package com.itlbv.routines.repository.mock;
+
+import org.junit.Test;
+
+public class MockRoutineRepositoryImplTest {
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
