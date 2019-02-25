80fa07fc93f97b1e38ed8034c36a8063803ebf23
itlbv <artem.v.fokin@gmail.com>
2019-02-25 15:18:53 +0100

    Add spring-app.xml and spring testing an Main

--- src/main/java/com/itlbv/routines/Main.java
+++ src/main/java/com/itlbv/routines/Main.java
@@ -1,14 +1,29 @@
 package com.itlbv.routines;
 
+import com.itlbv.routines.repository.RoutineRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.out.println("METHOD MAIN");
-        log.info("Example log");
+        log.info("Main method Spring testing");
+
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+
+        RoutineRepository routineRepository = appCtx.getBean(RoutineRepository.class);
+        System.out.println(routineRepository.getAll());
+
+        //RoutineService routineService = appCtx.getBean(RoutineService.class);
+        //routineService.create();
+
+        appCtx.close();
     }
 }
--- src/main/java/com/itlbv/routines/repository/mock/MockRoutineRepositoryImpl.java
+++ src/main/java/com/itlbv/routines/repository/mock/MockRoutineRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.itlbv.routines.repository.mock;
 
 import com.itlbv.routines.model.Routine;
 import com.itlbv.routines.repository.RoutineRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class MockRoutineRepositoryImpl implements RoutineRepository {
     private ConcurrentHashMap<Integer, Routine> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
@@ -33,7 +35,7 @@ public class MockRoutineRepositoryImpl implements RoutineRepository {
 
     @Override
     public List<Routine> getAll() {
-        return null;
+        return new ArrayList<>(repository.values());
     }
 
     @Override
@@ -47,24 +49,23 @@ public class MockRoutineRepositoryImpl implements RoutineRepository {
     }
 
     private void populateRepository() {
-        routinesForInitialPopulation.add(new Routine(1, "Routine 1", "Description 1",
+        routinesForInitialPopulation.add(new Routine("Routine 1", "Description 1",
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalTime.of(11, 0)));
-        routinesForInitialPopulation.add(new Routine(2, "Routine 2", "Description 2",
+        routinesForInitialPopulation.add(new Routine("Routine 2", "Description 2",
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalTime.of(11, 0)));
-        routinesForInitialPopulation.add(new Routine(3, "Routine 3", "Description 3",
+        routinesForInitialPopulation.add(new Routine("Routine 3", "Description 3",
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalTime.of(11, 0)));
-        routinesForInitialPopulation.add(new Routine(4, "Routine 4", "Description 4",
+        routinesForInitialPopulation.add(new Routine("Routine 4", "Description 4",
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 0),
                 LocalTime.of(11, 0)));
 
         routinesForInitialPopulation.forEach(this::save);
-        counter.set(repository.size());
     }
 }
